689769d2551570bbe938d30fdd8f0e63850b1f66
asmitsaha5 <141541424+asmitsaha5@users.noreply.github.com>
2024-05-12 21:33:17 +0530

    Add files via upload

---
+++ Basics level 1/1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a=2,b=3;
+printf("sum of a and b is %d\n",a+b);
+printf("product of a and b is %d\n",a*b);
+printf("modulus of a and b is %d\n",a%b);
+printf("difference of a and b is %d\n", a-b);
+printf("divisor of a and b is %d\n",a/b);
+}
---
+++ Basics level 1/3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define PI 3.14 
+void main()
+{
+int base=4;
+int height=8;
+int area=(base*height)/2;
+printf("Area of the triangle is = %d",area);
+int radius=8;
+int Area=PI*radius*radius;
+printf("Area of circle is =%d",Area);
+}
